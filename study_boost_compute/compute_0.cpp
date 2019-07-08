d96a87de9767a8a34a1066a7cd468a1f3b1aca28
linwei <zlinwei@zlinwei.com>
2019-07-08 17:42:01 +0800

    Open CL hello world;

---
+++ study_boost_compute/compute_0.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Administrator on 2019/7/8.
+//
+
+#include <iostream>
+
+#include <boost/compute/core.hpp>
+
+namespace compute = boost::compute;
+
+int main()
+{
+    // get the default device
+    compute::device device = compute::system::default_device();
+
+    // print the device's name and platform
+    std::cout << "hello from " << device.name();
+    std::cout << " (platform: " << device.platform().name() << ")" << std::endl;
+
+    return 0;
+}
