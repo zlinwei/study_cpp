057b85c807f6c42c1ae172ddf9c82385274310c2
linwei <zlinwei@zlinwei.com>
2019-07-11 18:01:27 +0800

    python plot

--- study_boost_python/python_plot.cpp
+++ study_boost_python/python_plot.cpp
@@ -2,40 +2,29 @@
 // Created by Administrator on 2019/7/11.
 //
 
-#include <boost/python.hpp>
+#include <Python.h>
+
 #include <glog/logging.h>
 
-namespace py = boost::python;
-
-wchar_t *GetWC(const char *c) {
-    const size_t cSize = strlen(c) + 1;
-    wchar_t *wc = new wchar_t[cSize];
-    mbstowcs(wc, c, cSize);
-
-    return wc;
-}
-
 int main() {
-    try {
-        Py_Initialize();
-        py::object main_module = py::import("__main__");
-        py::object main_namespace = main_module.attr("__dict__");
+	try {
+		Py_Initialize();
 
-        py::object res = py::exec("print(\"hello world\")", main_namespace);
-        py::object rand_mod = boost::python::import("random");
-        py::object rand_func = rand_mod.attr("random");
-        py::object rand2 = rand_func();
-        LOG(INFO) << py::extract<float>(rand2);
+		PyRun_SimpleString("import matplotlib.pyplot as plt");
+		PyRun_SimpleString("import numpy as np");
+        PyRun_SimpleString("plt.figure()");
+        PyRun_SimpleString("plt.plot([1,2,2,2]");
+		PyRun_SimpleString("plt.show()");
+		Py_Finalize();
 
-        py::object np = py::import("numpy");
+		LOG(INFO) << "hello";
+	}
+	catch (std::exception& e) {
+		LOG(ERROR) << e.what();
+	}
+	catch (...) {
+		LOG(ERROR) << "Uncaught exception";
+	}
 
-        LOG(INFO) << "hello";
-        Py_Finalize();
-    } catch (std::exception &e) {
-        LOG(ERROR) << e.what();
-    } catch (...){
-        LOG(ERROR) << "Uncaught exception";
-    }
-
-    return 0;
+	return 0;
 }
