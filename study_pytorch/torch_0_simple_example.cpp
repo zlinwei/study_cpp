0bcef694feb65f767d6433f781be1cdaf9f784fb
linwei <zlinwei@zlinwei.com>
2019-07-12 11:15:33 +0800

    compare python API and c++ API

--- study_pytorch/torch_0_simple_example.cpp
+++ study_pytorch/torch_0_simple_example.cpp
@@ -4,10 +4,39 @@
 
 #include <torch/torch.h>
 #include <iostream>
+#include <torch/torch.h>
+
+/*
+ * In Python, it would look like this:
+ import torch
+ class Net(torch.nn.Module):
+    def __init__(self, N, M):
+        super(Net, self).__init__()
+        self.W = torch.nn.Parameter(torch.randn(N, M))
+        self.b = torch.nn.Parameter(torch.randn(M))
+
+    def forward(self, input):
+        return torch.addmm(self.b, input, self.W)
+*/
+
+struct Net : torch::nn::Module {
+    Net(int64_t N, int64_t M) {
+        W = register_parameter("W", torch::randn({N, M}));
+        b = register_parameter("b", torch::randn(M));
+    }
+
+    torch::Tensor forward(torch::Tensor input) {
+        return torch::addmm(b, input, W);
+    }
+
+    torch::Tensor W, b;
+};
 
 int main() {
     torch::Tensor tensor = torch::eye(3);
     std::cout << tensor << std::endl;
-    
+
     return 0;
 }
+
+
