8a6231111da8891c704d9658d1fb506aaf84608d
linwei <zlinwei@zlinwei.com>
2019-07-12 11:03:24 +0800

    add pytorch lib

---
+++ study_pytorch/torch_0_simple_example.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Administrator on 2019/7/12.
+//
+
+#include <torch/torch.h>
+#include <iostream>
+
+int main() {
+    torch::Tensor tensor = torch::eye(3);
+    std::cout << tensor << std::endl;
+    
+    return 0;
+}
