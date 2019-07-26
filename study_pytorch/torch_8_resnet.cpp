2c5cff53eb0421857ef07f8c4a12343aab2f2295
linwei <zlinwei@zlinwei.com>
2019-07-26 22:27:10 +0800

    train large batch size that memory no support;

--- study_pytorch/torch_8_resnet.cpp
+++ study_pytorch/torch_8_resnet.cpp
@@ -31,36 +31,37 @@ using namespace torch;
 class ConvBlock2Impl : public torch::nn::Module {
 public:
     ConvBlock2Impl(int input_ch, int output_ch, int kernel_size) :
-            _conv1(register_module("conv1",nn::Conv2d(nn::Conv2dOptions(input_ch, output_ch, {kernel_size, kernel_size}).stride(1))
+            _conv1(register_module("conv1", nn::Conv2d(
+                    nn::Conv2dOptions(input_ch, output_ch, {kernel_size, kernel_size}).stride(1))
             )),
-			_bn1(register_module("bn1",
-				nn::BatchNorm(output_ch)
-				)),
+            _bn1(register_module("bn1",
+                                 nn::BatchNorm(output_ch)
+            )),
             _conv2(register_module("conv2", nn::Conv2d(
                     nn::Conv2dOptions(output_ch, output_ch, {kernel_size, kernel_size})
                             .stride(1))
             )),
-			_bn2(register_module("bn2",nn::BatchNorm(output_ch))){
+            _bn2(register_module("bn2", nn::BatchNorm(output_ch))) {
 
     }
 
     torch::Tensor forward(torch::Tensor &x) {
         x = _conv1->forward(x);
-		x = _bn1->forward(x);
-		x = torch::relu(x);
+        x = _bn1->forward(x);
+        x = torch::relu(x);
 
         x = _conv2->forward(x);
-		x = _bn2->forward(x);
-		x = torch::relu(x);
+        x = _bn2->forward(x);
+        x = torch::relu(x);
 
         return x;
     }
 
 private:
-	nn::Conv2d _conv1{ nullptr };
-	nn::BatchNorm _bn1{ nullptr };
-	nn::Conv2d _conv2{ nullptr };
-	nn::BatchNorm _bn2{ nullptr };
+    nn::Conv2d _conv1{nullptr};
+    nn::BatchNorm _bn1{nullptr};
+    nn::Conv2d _conv2{nullptr};
+    nn::BatchNorm _bn2{nullptr};
 };
 
 TORCH_MODULE(ConvBlock2);
@@ -105,57 +106,74 @@ private:
 TORCH_MODULE(ResNet18);
 
 
-
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         LOG(INFO) << "Usage :" << argv[0] << " [path]";
         return -1;
     }
 
-	try
-	{
-		ResNet18 resNet18{};
-		LOG(INFO) << "\n" << resNet18;
+    try {
+        ResNet18 resNet18{};
+        LOG(INFO) << "\n" << resNet18;
 
-		torch::optim::Adam generator_optimizer(
-			resNet18->parameters(), torch::optim::AdamOptions(2e-4).beta1(0.5));
-		torch::optim::Adam discriminator_optimizer(
-			resNet18->parameters(), torch::optim::AdamOptions(5e-4).beta1(0.5));
+        torch::optim::Adam generator_optimizer(
+                resNet18->parameters(), torch::optim::AdamOptions(2e-4).beta1(0.5));
+        torch::optim::Adam discriminator_optimizer(
+                resNet18->parameters(), torch::optim::AdamOptions(5e-4).beta1(0.5));
 
-		std::string path = argv[1];
-		auto dataset = HumanProteinAtlasDataset(path, 1000).map(torch::data::transforms::Stack<>());
-		LOG(INFO) << "data size: " << *dataset.size();
-		
-		// data loader
-		auto data_loader = torch::data::make_data_loader<>(std::move(dataset),
-			torch::data::DataLoaderOptions().batch_size(8).workers(4));
+        std::string path = argv[1];
+//        auto dataset = HumanProteinAtlasDataset(path).map(torch::data::transforms::Stack<>());
+        auto dataset = HumanProteinAtlasDataset(path, 1000);
+        LOG(INFO) << "data size: " << *dataset.size();
 
-		LOG(INFO) << "data loader created";
+        // data loader
+//        auto data_loader = torch::data::make_data_loader<>(std::move(dataset),
+//                                                           torch::data::DataLoaderOptions().batch_size(1).workers(4));
 
+        LOG(INFO) << "data loader created";
 
-		for (int64_t epoch = 1; epoch <= 10; ++epoch) {
-			int64_t batch_index = 0;
-			for (auto& batch : *data_loader) {
-				LOG(INFO) << "Batch size: " << batch.data.sizes() << " | Labels: "
-					<< batch.target.sizes() << std::endl;
+        const int batch_size = 64;
+        for (int64_t epoch = 1; epoch <= 10; ++epoch) {
+            const size_t total_samp = dataset.size().value();
+            size_t samp_index = 0;
+            while (true) {
+                for (int i = 0; i < batch_size; ++i) {
+                    auto data = dataset.get(samp_index);
+                    auto image = data.data.view({1, 1, 512, 512});
+                    auto label = data.target.view({1, 1, 28});
+                    torch::Tensor real_output = resNet18->forward(image);
+                    torch::Tensor d_loss_real = torch::binary_cross_entropy(real_output, label);
+                    d_loss_real.backward();
+                    LOG(INFO) << d_loss_real;
+                    samp_index++;
+                    if (samp_index > total_samp)break;
+                }
 
-				// Train resnet with real images.
-				resNet18->zero_grad();
-				auto real_images = batch.data;
-				auto real_labels = batch.target;
-				torch::Tensor real_output = resNet18->forward(real_images);
-				torch::Tensor d_loss_real = torch::binary_cross_entropy(real_output, real_labels);
-				LOG(INFO) << d_loss_real;
-				d_loss_real.backward();
+                LOG(INFO) << "zero grad";
+                resNet18->zero_grad();
 
-				LOG(INFO) << batch_index++;
-			}
-		}
-	}
-	catch (const std::exception& e)
-	{
-		LOG(INFO) << e.what();
-	}
+                if (samp_index > total_samp)break;
+            }
+//            for (auto &batch : *data_loader) {
+//                LOG(INFO) << "Batch size: " << batch.data.sizes() << " | Labels: "
+//                          << batch.target.sizes() << std::endl;
+//
+//                // Train resnet with real images.
+//                resNet18->zero_grad();
+//                auto real_images = batch.data;
+//                auto real_labels = batch.target;
+//                torch::Tensor real_output = resNet18->forward(real_images);
+//                torch::Tensor d_loss_real = torch::binary_cross_entropy(real_output, real_labels);
+//                LOG(INFO) << d_loss_real;
+//                d_loss_real.backward();
+//
+//                LOG(INFO) << batch_index++;
+//            }
+        }
+    }
+    catch (const std::exception &e) {
+        LOG(INFO) << e.what();
+    }
 
     return 0;
 }
