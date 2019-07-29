3f5e5f592142b72c5d478f4c9822735167016344
linwei <zlinwei@zlinwei.com>
2019-07-29 21:35:03 +0800

    tree level order and zigzag level order;

---
+++ study_algorithm/alg_69_tree_level_order_traversal.cpp
@@ -0,0 +1,81 @@
+//
+// Created by linwei on 2019-07-29.
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left, *right;
+
+    explicit TreeNode(int val) {
+        this->val = val;
+        this->left = this->right = NULL;
+    }
+};
+
+
+class Solution {
+public:
+    /**
+     * @param root: A Tree
+     * @return: Level order a list of lists of integer
+     */
+    vector<vector<int>> levelOrder(TreeNode *root) {
+        // write your code here
+        vector<vector<int>> output;
+        queue<TreeNode *> q;
+        if (root)
+            q.push(root);
+
+        int i = 1, j = 0;
+        vector<int> current;
+        while (!q.empty()) {
+            auto tmp = q.front();
+            current.push_back(tmp->val);
+            if (tmp->left) {
+                q.push(tmp->left);
+                j++;
+            }
+            if (tmp->right) {
+                q.push(tmp->right);
+                j++;
+            }
+            i--;
+            q.pop();
+            if (i == 0) {
+                output.push_back(current);
+                current.clear();
+                i = j;
+                j = 0;
+            }
+
+        }
+
+
+        return output;
+    }
+
+};
+
+int main(int, char *[]) {
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+
+    Solution solution{};
+    auto result = solution.levelOrder(root);
+    for (auto item : result) {
+        std::cout << "[";
+        for (size_t i = 0; i < item.size(); ++i) {
+            std::cout << item[i] << ",";
+        }
+        std::cout << "]," << std::endl;
+    }
+    std::cout << endl;
+}
