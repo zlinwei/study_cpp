0c7e7c0060ab04fb29ddb4616fd2d5830ae165ed
linwei <zlinwei@zlinwei.com>
2019-07-30 17:35:42 +0800

    car dataset get filename;

--- study_pytorch/CarDataset.hpp
+++ study_pytorch/CarDataset.hpp
@@ -6,12 +6,22 @@
 #define STUDY_CPP_CARDATASET_HPP
 
 #include <torch/torch.h>
-
+#include <opencv2/opencv.hpp>
 using namespace torch;
 
 class CarDataItem{
 public:
+    const string &getFilename() const {
+        return _filename;
+    }
 
+    const string &getLabelStr() const {
+        return _label_str;
+    }
+
+private:
+    std::string _filename; //like 1.jpg
+    std::string _label_str; //like 0 0
 };
 
 class CarDataset :public torch::data::Dataset<CarDataset>{
@@ -21,7 +31,9 @@ public:
     }
 
     ExampleType get(size_t index) override {
-        
+        auto &item = _data.at(index);
+        auto img = cv::imread(item.getFilename());
+
     }
 
 
