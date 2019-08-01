be6f0224344c2e4554e71c465a2d91515837c207
linwei <zlinwei@zlinwei.com>
2019-08-01 22:21:17 +0800

    define a Car classification net;

--- study_pytorch/CarDataset.hpp
+++ study_pytorch/CarDataset.hpp
@@ -39,7 +39,40 @@ private:
 
 };
 
-class CarNet : public torch::nn::Module{
+class CarNet : public torch::nn::Module {
+public:
+    CarNet(int ch, int classes) {
+        _conv0 = register_module("conv_0", nn::Conv2d(nn::Conv2dOptions(ch, 20, {5, 5})));
+        _conv1 = register_module("conv_1", nn::Conv2d(nn::Conv2dOptions(20, 40, {4, 4})));
+        _conv2 = register_module("conv_2", nn::Conv2d(nn::Conv2dOptions(40, 60, {3, 3})));
+        _fc0 = register_module("fc0", nn::Linear(60, 100));
+        _fc1 = register_module("fc1", nn::Linear(100, classes));
+    }
+
+    torch::Tensor forward(torch::Tensor x) {
+        x = _conv0->forward(x);
+        x = torch::relu(x);
+        x = torch::max_pool2d(x, {2, 2}, {2, 2});
+        x = _conv1->forward(x);
+        x = torch::relu(x);
+        x = torch::max_pool2d(x, {2, 2}, {2, 2});
+        x = _conv2->forward(x);
+        x = torch::relu(x);
+        x = torch::max_pool2d(x, {2, 2}, {2, 2});
+        x = torch::adaptive_avg_pool2d(x, {1, 1});
+        x = x.view({x.size(0), x.size(1)});
+        x = _fc0->forward(x);
+        x = torch::dropout(x, 0.5, is_training());
+        x = _fc1->forward(x);
+        return x;
+    }
+
+private:
+    torch::nn::Conv2d _conv0{nullptr}; //chan=20,kernel_size=5,relu, max_pool 2x2 2x2
+    torch::nn::Conv2d _conv1{nullptr}; //chan=40,kernel_size=4,relu, max_pool 2x2 2x2
+    torch::nn::Conv2d _conv2{nullptr}; //chan=60,kernel_size=3,relu, max_pool 2x2 2x2
+    torch::nn::Linear _fc0{nullptr}; //output=100,dropout
+    torch::nn::Linear _fc1{nullptr};
 
 };
 
