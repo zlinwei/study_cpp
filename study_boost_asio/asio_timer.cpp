c89fde694a79e70d0fc37ca5db0eda0e73150476
linwei <zlinwei@zlinwei.com>
2019-08-31 19:33:54 +0800

    boost asio timer and poll

--- study_boost_asio/asio_timer.cpp
+++ study_boost_asio/asio_timer.cpp
@@ -42,8 +42,34 @@ void test_async_deadline_timer() {
     io.run_one();
 }
 
+void test_async_deadline_poll() {
+    boost::asio::io_context io{};
+    boost::asio::deadline_timer timer(io);
+    timer.expires_from_now(boost::posix_time::seconds(5));
+
+    show_current_time("before async_wait:");
+
+    timer.async_wait([](const boost::system::error_code &ec) {
+        if (!ec)
+            show_current_time("wait finished:");
+    });
+
+    show_current_time("after async_wait:");
+    auto now = std::chrono::high_resolution_clock::now();
+    while (std::chrono::high_resolution_clock::now() < now + std::chrono::seconds(10)) {
+        int events = io.poll();
+        if (events == 0)continue;
+        std::cout << events << std::endl;
+        show_current_time("poll:");
+    }
+
+    show_current_time("exit:");
+
+}
+
 int main(int argc, char *argv[]) {
-    test_deadline_timer();
-    test_async_deadline_timer();
+//    test_deadline_timer();
+//    test_async_deadline_timer();
+    test_async_deadline_poll();
     return 0;
 }
